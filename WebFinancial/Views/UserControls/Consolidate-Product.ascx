913856eedc026dd082bd0681a53abbbf8082a276
Sergio <chancaysergio@gmail.com>
2014-03-07 11:15:19 -0800

    Ajuste de referencias

--- WebFinancial/Views/UserControls/Consolidate-Product.ascx
+++ WebFinancial/Views/UserControls/Consolidate-Product.ascx
@@ -3,6 +3,94 @@
 
 <asp:Literal ID="ltrChart" runat="server"></asp:Literal>
 
-<%--<script type="text/javascript">
-    jQuery.cssProps.opacity = 'opacity';
-</script>--%>
+<div id="container1"></div>
+<div id="container2"></div>
+<script type="text/javascript">
+    // Internationalization
+    Highcharts.setOptions({
+        lang: {
+            drillUpText: '◁ Back to {series.name}'
+        }
+    });
+
+    var options = {
+        chart: {
+            height: 300
+        },
+
+        title: {
+            text: 'Highcharts Drilldown Plugin'
+        },
+
+        xAxis: {
+            categories: true
+        },
+
+        drilldown: {
+            series: [{
+                id: 'fruits',
+                name: 'Fruits',
+                data: [
+                    ['Apples', 4],
+                    ['Pears', 6],
+                    ['Oranges', 2],
+                    ['Grapes', 8]
+                ]
+            }, {
+                id: 'cars',
+                name: 'Cars',
+                data: [{
+                    name: 'Toyota',
+                    y: 4,
+                    drilldown: 'toyota'
+                },
+                ['Volkswagen', 3],
+                ['Opel', 5]
+                ]
+            }, {
+                id: 'toyota',
+                name: 'Toyota',
+                data: [
+                    ['RAV4', 3],
+                    ['Corolla', 1],
+                    ['Carina', 4],
+                    ['Land Cruiser', 5]
+                ]
+            }]
+        },
+
+        legend: {
+            enabled: false
+        },
+
+        plotOptions: {
+            series: {
+                dataLabels: {
+                    enabled: true
+                },
+                shadow: false
+            },
+            pie: {
+                size: '80%'
+            }
+        },
+
+        series: [{
+            name: 'Overview',
+            colorByPoint: true,
+            data: <%=demo %>
+            }]
+    };
+
+    // Column chart
+    options.chart.renderTo = 'container1';
+    options.chart.type = 'column';
+    var chart1 = new Highcharts.Chart(options);
+
+    // Pie
+    options.chart.renderTo = 'container2';
+    options.chart.type = 'pie';
+    var chart2 = new Highcharts.Chart(options);
+
+
+</script>
--- WebFinancial/Views/UserControls/Consolidate-Product.ascx.cs
+++ WebFinancial/Views/UserControls/Consolidate-Product.ascx.cs
@@ -23,89 +23,106 @@ public partial class Views_UserControls_Consolidate_Product : System.Web.UI.User
 
     protected void Render_Chart()
     {
+        string[] dtCategories = new[] { "Checking", "Saving" };
+        Data dtAvailable = new Data(new object[] { 12 });
+        Data dtCurrent = new Data(new object[] { -2 });
+        Data dttemp = new Data(new[] { 
+            new DotNet.Highcharts.Options.Point { 
+                Y = 4455,
+                Color= Color.SkyBlue,  
+                Drilldown = new Drilldown { 
+                    Categories=new[]{"1111","2222","1122"}, 
+                    Name = "Account 1",
+                    Data=new Data(new object[]{1.2,2.3,22.35})}
+            },
+            new DotNet.Highcharts.Options.Point { 
+                Y = 7777,
+                Color= Color.SkyBlue,  
+                Drilldown = new Drilldown { 
+                    Categories=new[]{"3333","4444"}, 
+                    Name = "Account 2",
+                    Data=new Data(new object[]{33,334})}
+            }
+        });
+
         Highcharts chart = new Highcharts("chart")
-       .InitChart(new DotNet.Highcharts.Options.Chart()
-       {
-           DefaultSeriesType = DotNet.Highcharts.Enums.ChartTypes.Column
-       })
-       .SetLegend(new DotNet.Highcharts.Options.Legend()
-       {
-           Shadow = true
-       })
+       .InitChart(new DotNet.Highcharts.Options.Chart() { DefaultSeriesType = DotNet.Highcharts.Enums.ChartTypes.Column })
+       .SetLegend(new DotNet.Highcharts.Options.Legend() { Enabled = false, Shadow = true })
        .SetCredits(new Credits() { Enabled = false })
+       .SetTitle(new DotNet.Highcharts.Options.Title { Text = "Consolidate Posicion" })
+       .SetSubtitle(new Subtitle() { Text = "Detail of accounts from client." })
        .SetTooltip(new Tooltip()
        {
            Enabled = true,
-           HeaderFormat = "<span style=\"font-size:13px\">{point.key}</span><table>",
-           PointFormat = "<tr><td style=\"color:{series.color};padding:0\"><b>{series.name}:</b> ${point.y:.2f}</td></tr>",
+           HeaderFormat = "<span style=\"font-size:13px\">{series.name}</span><table>",
+           PointFormat = "<tr><td style=\"color:{series.color};padding:0\"><b>{point.name}:</b> ${point.y:.2f}</td></tr>",
            FooterFormat = "</table>",
            ValueSuffix = " Usd",
            UseHTML = true,
            HideDelay = 0,
            Shared = true
        })
-       .SetTitle(new DotNet.Highcharts.Options.Title
-       {
-           Text = "Consolidate Posicion"
-       })
-       .SetSubtitle(new Subtitle() { Text = "Detail of accounts from client." })
        .SetPlotOptions(new PlotOptions()
        {
-           Columnrange = new PlotOptionsColumnrange()
-           {
-               Animation = new Animation(false),
-               DataLabels = new PlotOptionsColumnrangeDataLabels()
-               {
-                   Enabled = true
-               }
-           },
+           Columnrange = new PlotOptionsColumnrange() { Animation = new Animation(false), DataLabels = new PlotOptionsColumnrangeDataLabels() { Enabled = true } },
            Column = new PlotOptionsColumn()
            {
+               Shadow = false,
+               Cursor = Cursors.Pointer,
+               Point = new PlotOptionsColumnPoint { Events = new PlotOptionsColumnPointEvents { Click = "ColumnPointClick" } },
                Animation = new Animation(new AnimationConfig { Duration = 500, Easing = EasingTypes.EaseOutBounce }),
                DataLabels = new PlotOptionsColumnDataLabels()
                {
                    Enabled = true,
-                   Rotation = -90,
-                   Align = DotNet.Highcharts.Enums.HorizontalAligns.Center,
-                   Color = Color.White,
-                   X = 4,
-                   Y = 10,
                    Formatter = @"function() {return '$ '+this.point.y;}",
-                   Style = "fontSize: '13px', fontFamily: 'Verdana, sans-serif', textShadow: '0 0 3px black'"
+                   Style = "fontWeight: 'bold'"
                }
            }
        })
-       .SetXAxis(new XAxis
-       {
-           Title = null,
-           StartOnTick = true,
-           EndOnTick = true,
-           ShowLastLabel = true,
-           Categories = new[] { "Account 1", "Account 2", "Account 3" },
-           Labels = new XAxisLabels()
-           {
-               Rotation = -45,
-               Align = DotNet.Highcharts.Enums.HorizontalAligns.Right,
-               Enabled = true,
-               Style = "fontSize: '11px', fontFamily: 'Verdana, sans-serif'"
-           }
-       })
-       .SetYAxis(new YAxis
-       {
-           Title = new YAxisTitle { Text = String.Empty }
-       })
-       .SetSeries(new DotNet.Highcharts.Options.Series[]{
-            new DotNet.Highcharts.Options.Series {
-                Name = "Available",
-                Data = new Data(new object[] { 29.9, 71.5, 106.4 })
-            },
-            
-            new DotNet.Highcharts.Options.Series {
-                Name = "Current",
-                Data = new Data(new object[] { 33, -52, 21.25 })
-            }
-        });
+       .SetXAxis(new XAxis { Categories = dtCategories })
+       .SetYAxis(new YAxis { Title = new YAxisTitle { Text = String.Empty } })
+       .SetSeries(new DotNet.Highcharts.Options.Series[] { new DotNet.Highcharts.Options.Series { Name = "Available", Data = dttemp } })
+       .AddJavascripFunction("ColumnPointClick",
+                    @"var drilldown = this.drilldown;
+                      if (drilldown) { // drill down
+                        setChart(drilldown.name, drilldown.categories, drilldown.data.data, drilldown.color);
+                      } else { // restore
+                        setChart(name, categories, data.data);
+                      }")
+        .AddJavascripFunction("setChart",
+                    @"chart.xAxis[0].setCategories(categories);
+                      chart.series[0].remove();
+                      chart.addSeries({
+                         name: name,
+                         data: data,
+                         color: color || 'white'
+                      });",
+                    "name", "categories", "data", "color"
+                )
+        .AddJavascripVariable("colors", "Highcharts.getOptions().colors")
+        .AddJavascripVariable("name", "'{0}'".FormatWith("demo chart"))
+        .AddJavascripVariable("categories", JsonSerializer.Serialize(dtCategories))
+        .AddJavascripVariable("data", JsonSerializer.Serialize(dttemp));
 
         ltrChart.Text = chart.ToHtmlString();
+    }
+
+    public Data demo
+    {
+        get
+        {
+            return new Data(new[] { 
+            new DotNet.Highcharts.Options.Point { 
+                Y = 10,
+                Name="Fruits",
+                Color= Color.SkyBlue,  
+                Drilldown = new Drilldown { 
+                    Categories=new[]{"1111","2222","1122"}, 
+                    Name = "fruits",
+                    Data=new Data(new object[]{1.2,2.3,22.35})
+                }
+            }
+            });
+        }
     }
 }
