ba1e591b6851c8dd072e1e2034bf05173d0aec82
Sergio <chancaysergio@gmail.com>
2014-03-06 19:38:12 -0800

    Actualizacion a la version-3.0.9

--- WebFinancial/Views/UserControls/Consolidate-Product.ascx
+++ WebFinancial/Views/UserControls/Consolidate-Product.ascx
@@ -2,3 +2,7 @@
 <%@ Register Assembly="System.Web.DataVisualization, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35" Namespace="System.Web.UI.DataVisualization.Charting" TagPrefix="asp" %>
 
 <asp:Literal ID="ltrChart" runat="server"></asp:Literal>
+
+<%--<script type="text/javascript">
+    jQuery.cssProps.opacity = 'opacity';
+</script>--%>
--- WebFinancial/Views/UserControls/Consolidate-Product.ascx.cs
+++ WebFinancial/Views/UserControls/Consolidate-Product.ascx.cs
@@ -1,4 +1,5 @@
 ﻿using DotNet.Highcharts;
+using DotNet.Highcharts.Enums;
 using DotNet.Highcharts.Helpers;
 using DotNet.Highcharts.Options;
 using System;
@@ -12,82 +13,94 @@ using System.Web.UI.WebControls;
 
 public partial class Views_UserControls_Consolidate_Product : System.Web.UI.UserControl
 {
+    protected void Page_InitComplete(object sender, EventArgs e)
+    {
+    }
     protected void Page_Load(object sender, EventArgs e)
     {
+        Render_Chart();
+    }
+
+    protected void Render_Chart()
+    {
         Highcharts chart = new Highcharts("chart")
-        .InitChart(new DotNet.Highcharts.Options.Chart()
-        {
-            DefaultSeriesType = DotNet.Highcharts.Enums.ChartTypes.Column
-        })
-        .SetLegend(new DotNet.Highcharts.Options.Legend()
-        {
-            Shadow = true
-        })
-        .SetTooltip(new Tooltip()
-        {
-            Enabled = true,
-            ValueSuffix = " Us",
-            PointFormat = "$ {point.y:.2f}",
-            UseHTML = true,
-            HideDelay = 0,
-            Shared = true
-        })
-        .SetTitle(new DotNet.Highcharts.Options.Title
-        {
-            Text = "Consolidate Posicion"
-        })
-        .SetSubtitle(new Subtitle() { Text = "Detail of accounts from client." })
-        .SetPlotOptions(new PlotOptions()
-        {
-            Columnrange = new PlotOptionsColumnrange()
-            {
-                DataLabels = new PlotOptionsColumnrangeDataLabels()
-                {
-                    Enabled = true
-                }
-            },
-            Column = new PlotOptionsColumn()
-            {
-                DataLabels = new PlotOptionsColumnDataLabels()
-                {
-                    Enabled = true,
-                    Rotation = -90,
-                    Align = DotNet.Highcharts.Enums.HorizontalAligns.Center,
-                    Color = Color.White,
-                    X = 4,
-                    Y = 10,
-                    Formatter = @"function() {return '$ '+this.point.y;}",
-                    Style = "fontSize: '13px', fontFamily: 'Verdana, sans-serif', textShadow: '0 0 3px black'"
-                }
-            }
-        })
-        .SetXAxis(new XAxis
-        {
-            StartOnTick = true,
-            EndOnTick = true,
-            ShowLastLabel = true,
-            Categories = new[] { "Account 1", "Account 2", "Account 3" },
-            Labels = new XAxisLabels()
-            {
-                Rotation = -45,
-                Align = DotNet.Highcharts.Enums.HorizontalAligns.Right,
-                Enabled = true,
-                Style = "fontSize: '11px', fontFamily: 'Verdana, sans-serif'"
-            }
-        })
-        .SetYAxis(new YAxis
-        {
-            Title = new YAxisTitle { Text = String.Empty }
-        })
-        .SetSeries(new DotNet.Highcharts.Options.Series[]{
+       .InitChart(new DotNet.Highcharts.Options.Chart()
+       {
+           DefaultSeriesType = DotNet.Highcharts.Enums.ChartTypes.Column
+       })
+       .SetLegend(new DotNet.Highcharts.Options.Legend()
+       {
+           Shadow = true
+       })
+       .SetCredits(new Credits() { Enabled = false })
+       .SetTooltip(new Tooltip()
+       {
+           Enabled = true,
+           HeaderFormat = "<span style=\"font-size:13px\">{point.key}</span><table>",
+           PointFormat = "<tr><td style=\"color:{series.color};padding:0\"><b>{series.name}:</b> ${point.y:.2f}</td></tr>",
+           FooterFormat = "</table>",
+           ValueSuffix = " Usd",
+           UseHTML = true,
+           HideDelay = 0,
+           Shared = true
+       })
+       .SetTitle(new DotNet.Highcharts.Options.Title
+       {
+           Text = "Consolidate Posicion"
+       })
+       .SetSubtitle(new Subtitle() { Text = "Detail of accounts from client." })
+       .SetPlotOptions(new PlotOptions()
+       {
+           Columnrange = new PlotOptionsColumnrange()
+           {
+               Animation = new Animation(false),
+               DataLabels = new PlotOptionsColumnrangeDataLabels()
+               {
+                   Enabled = true
+               }
+           },
+           Column = new PlotOptionsColumn()
+           {
+               Animation = new Animation(new AnimationConfig { Duration = 500, Easing = EasingTypes.EaseOutBounce }),
+               DataLabels = new PlotOptionsColumnDataLabels()
+               {
+                   Enabled = true,
+                   Rotation = -90,
+                   Align = DotNet.Highcharts.Enums.HorizontalAligns.Center,
+                   Color = Color.White,
+                   X = 4,
+                   Y = 10,
+                   Formatter = @"function() {return '$ '+this.point.y;}",
+                   Style = "fontSize: '13px', fontFamily: 'Verdana, sans-serif', textShadow: '0 0 3px black'"
+               }
+           }
+       })
+       .SetXAxis(new XAxis
+       {
+           Title = null,
+           StartOnTick = true,
+           EndOnTick = true,
+           ShowLastLabel = true,
+           Categories = new[] { "Account 1", "Account 2", "Account 3" },
+           Labels = new XAxisLabels()
+           {
+               Rotation = -45,
+               Align = DotNet.Highcharts.Enums.HorizontalAligns.Right,
+               Enabled = true,
+               Style = "fontSize: '11px', fontFamily: 'Verdana, sans-serif'"
+           }
+       })
+       .SetYAxis(new YAxis
+       {
+           Title = new YAxisTitle { Text = String.Empty }
+       })
+       .SetSeries(new DotNet.Highcharts.Options.Series[]{
             new DotNet.Highcharts.Options.Series {
-                Type = DotNet.Highcharts.Enums.ChartTypes.Column,
                 Name = "Available",
                 Data = new Data(new object[] { 29.9, 71.5, 106.4 })
             },
             
             new DotNet.Highcharts.Options.Series {
-                Type = DotNet.Highcharts.Enums.ChartTypes.Column,
                 Name = "Current",
                 Data = new Data(new object[] { 33, -52, 21.25 })
             }
