6d6488630d62e34529423802e2d4d0bf4301b802
Sergio <chancaysergio@gmail.com>
2014-03-12 11:16:20 -0500

    Integracion de Clase Compartida Constantes,enum,etc..

---
+++ Shared.Commons/UtilsConstants.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Commons
+{
+    public class UtilsConstants
+    {
+        public enum TypeProductsFilters { All, Actives, Pasives, Saving, Cheking, Loans }
+
+        public const int PRODUCT_SAVING_ID = 4;
+        public const int PRODUCT_CHECKING_ID = 3;
+        public const int PRODUCT_LOANS_ID = 7;
+
+        public static int[] ProductsActives = new int[] { PRODUCT_SAVING_ID, PRODUCT_CHECKING_ID };
+        public static int[] ProductsPasives = new int[] { PRODUCT_LOANS_ID };
+    }
+}
