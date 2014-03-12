8cdaabfbb0d9cd12a8a6f7227be5fc2609152331
Sergio <chancaysergio@gmail.com>
2014-03-12 11:17:00 -0500

    Integracion de Summario de metodos

--- Adapters.DataAccess.DataTest/ProductsAdapters.cs
+++ Adapters.DataAccess.DataTest/ProductsAdapters.cs
@@ -10,25 +10,79 @@ namespace Adapters.DataAccess.Managment
 {
     public class ProductsAdapters : IProductsAdapters
     {
+        List<Product> dataProducts = new List<Product>(new[] { 
+            new Product() { Account = "123456789", AmmountAvailable = 12.2, Currency = 4, Name = "Cuenta Amiga 1", Type = 4 },
+            new Product() { Account = "589663254", AmmountAvailable = 256.32, Currency = 4, Name = "Cuenta Amiga 2", Type = 3 },
+            new Product() { Account = "588963211", AmmountAvailable = 4000.2, Currency = 4, Name = "Cuenta Amiga 3", Type = 7 },
+            new Product() { Account = "588964555", AmmountAvailable = 122.2, Currency = 4, Name = "Cuenta Amiga 4", Type = 4 } 
+        });
+
+        List<ProductMovements> dataMovements = new List<ProductMovements>(new[]{
+            new ProductMovements() { Date = DateTime.Parse("2014-09-10"), AmmountTransfer = 1200, Description = "Transferencia a Familia", TypeTransfer = "D" },
+            new ProductMovements() { Date = DateTime.Parse("2014-10-30"), AmmountTransfer = 200, Description = "Pago de Luz", TypeTransfer = "D" },
+            new ProductMovements() { Date = DateTime.Parse("2014-01-15"), AmmountTransfer = 12, Description = "Almuerzo de Homero", TypeTransfer = "D" },
+            new ProductMovements() { Date = DateTime.Parse("2014-06-25"), AmmountTransfer = 55.32, Description = "Prestamo banco", TypeTransfer = "D" },
+            new ProductMovements() { Date = DateTime.Parse("2014-03-02"), AmmountTransfer = 0.25, Description = "Impuesto de Amazon", TypeTransfer = "D" }
+        });
+
+        /// <summary>
+        /// Get All Products
+        /// </summary>
+        /// <returns></returns>
         public IEnumerable<Product> GetProducts()
         {
-            List<Product> data = new List<Product>();
-            data.Add(new Product() { Account = "123456789", AmmountAvailable = 12.2, Currency = 4, Name = "Cuenta Amiga 1", Type = 4 });
-            data.Add(new Product() { Account = "589663254", AmmountAvailable = 256.32, Currency = 4, Name = "Cuenta Amiga 2", Type = 3 });
-            data.Add(new Product() { Account = "588963211", AmmountAvailable = 4000.2, Currency = 4, Name = "Cuenta Amiga 3", Type = 7 });
-            data.Add(new Product() { Account = "588964555", AmmountAvailable = 122.2, Currency = 4, Name = "Cuenta Amiga 4", Type = 4 });
-
-            return data;
+            return dataProducts;
         }
+
+        /// <summary>
+        /// Get All Movements
+        /// </summary>
+        /// <returns></returns>
         public IEnumerable<ProductMovements> GetMovements()
         {
-            List<ProductMovements> data = new List<ProductMovements>();
-            data.Add(new ProductMovements() { Date = DateTime.Parse("3/9/2014"), AmmountTransfer = 1200, Description = "Transferencia a Familia", TypeTransfer = "D" });
-            data.Add(new ProductMovements() { Date = DateTime.Parse("5/10/2014"), AmmountTransfer = 200, Description = "Pago de Luz", TypeTransfer = "D" });
-            data.Add(new ProductMovements() { Date = DateTime.Parse("10/10/2014"), AmmountTransfer = 12, Description = "Almuerzo de Homero", TypeTransfer = "D" });
-            data.Add(new ProductMovements() { Date = DateTime.Parse("13/12/2014"), AmmountTransfer = 55.32, Description = "Prestamo banco", TypeTransfer = "D" });
-            data.Add(new ProductMovements() { Date = DateTime.Parse("23/12/2014"), AmmountTransfer = 0.25, Description = "Impuesto de Amazon", TypeTransfer = "D" });
-            return data;
+            return dataMovements;
+        }
+
+        /// <summary>
+        /// Get Products by Product type.
+        /// </summary>
+        /// <param name="ProductType"></param>
+        /// <returns></returns>
+        public IEnumerable<Product> GetProducts(int ProductType)
+        {
+            return dataProducts.Where(x => x.Type == ProductType);
+        }
+
+        /// <summary>
+        /// Get Products by array of Product Type
+        /// </summary>
+        /// <param name="ProductType"></param>
+        /// <returns></returns>
+        public IEnumerable<Product> GetProducts(int[] ProductType)
+        {
+            return dataProducts.Where(x => ProductType.Contains(x.Type));
+        }
+
+        /// <summary>
+        /// Get Products by Product Type and Currency Type.
+        /// </summary>
+        /// <param name="ProductType"></param>
+        /// <param name="CurrencyType"></param>
+        /// <returns></returns>
+        public IEnumerable<Product> GetProducts(int ProductType, int CurrencyType)
+        {
+            return dataProducts.Where(x => x.Type == ProductType && x.Currency == CurrencyType);
+        }
+
+        /// <summary>
+        /// Get Products by array of Product Type and Currency Type.
+        /// </summary>
+        /// <param name="ProductType"></param>
+        /// <param name="CurrencyType"></param>
+        /// <returns></returns>
+        public IEnumerable<Product> GetProducts(int[] ProductType, int CurrencyType)
+        {
+            return dataProducts.Where(x => ProductType.Contains(x.Type) && x.Currency == CurrencyType);
         }
     }
 }
